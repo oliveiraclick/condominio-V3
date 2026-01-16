4a6d1e6f8512eb1cc2a0ac62b89ac54f0af41c8e
Denys <ia.oliveira.click@gmail.com>
2026-01-15 21:55:44 -0300

    fix: Atualiza Bundle ID para app.morador.app para corresponder ao Apple Developer

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.oliveiraclick.appmorador',
+  appId: 'app.morador.app',
   appName: 'App Morador',
   webDir: 'dist',
   plugins: {
